17df97a341adaa40779254385ede5c44466cb96f
Vladimir Tyalo <bobtial@inbox.ru>
2016-12-02 11:27:52 +0400

    make slider markup adjust slider, about and plans towards pixel perfect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
+// import Slider from './slider.js';
+
 $(() => {
 	svg4everybody();
 });
---
+++ app/scripts/slider.js
@@ -0,0 +1,13 @@
+// import $ from 'jquery';
+//
+//
+// function Slider(options) {
+// 	const {element, steps} = options;
+//
+//
+//
+//
+//
+// }
+//
+// export default Slider;
